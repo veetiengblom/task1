93cbec5d7d0c0a450105cc2cf624ab921797261a
Veeti Engblom <veetiengblom@eduroam-cl353.wl.lut.fi>
2022-03-10 15:21:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button(View v) {
+        System.out.println("Hello world!");
+    }
+
+}
