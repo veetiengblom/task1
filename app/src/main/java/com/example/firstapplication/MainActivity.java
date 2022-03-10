ad044d15bc659a62d29f97debfe4c9fd6b775b94
veetiengblom <Veeti_engblom@hotmail.com>
2022-03-10 16:23:18 +0200

    Modify to task 4

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.text.TextWatcher;
+import android.text.Editable;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements TextWatcher{
 
     String userText;
+
     TextView text;
 
     EditText userInput;
+    EditText EditTextOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         text = (TextView) findViewById(R.id.textView);
         text.setText("Replace text!");
         userInput = (EditText) findViewById(R.id.userInput);
+        EditTextOut = (EditText) findViewById(R.id.userInput);
+        EditTextOut.addTextChangedListener(this);
+    }
+
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    public void afterTextChanged(Editable s) {
+        text.setText(s);
     }
 
     public void button(View v) {
@@ -32,5 +51,4 @@ public class MainActivity extends AppCompatActivity {
         userText = userInput.getText().toString();
         text.setText(userText);
     }
-
 }
