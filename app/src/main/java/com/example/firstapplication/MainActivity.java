7739e106b36ce4c1d27d77e8ac8f65406e85116e
veetiengblom <Veeti_engblom@hotmail.com>
2022-03-10 15:32:51 +0200

    Modify to task 2

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.firstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textView);
+        text.setText("Replace text!");
     }
 
     public void button(View v) {
         System.out.println("Hello world!");
+        text.setText("Hello world!");
     }
 
 }
